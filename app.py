afd7b08352ba1fc5b55dd6f2b6e8dd359ab527bf
Jordan Holtkamp <jordanholtkamp@gmail.com>
2020-04-08 22:17:17 -0400

    continue config

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+
 # db = SQLAlchemy(app)
 
 @app.route('/')
 @app.route('/api/v1')
 def home_page():
   return "Honey I'm home and I have an API"
+
 
 
 if __name__ == '__main__':
