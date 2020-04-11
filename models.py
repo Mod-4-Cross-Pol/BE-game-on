86249c483ae3b2db08e0bb3af66e61d6d7afec9b
Phil DeLong <philjdelong@gmail.com>
2020-04-11 14:42:32 -0600

    Update event model

--- models.py
+++ models.py
@@ -10,22 +10,28 @@ class Event(db.Model):
   __tablename__ = 'events'
 
   id = db.Column(db.Integer, primary_key=True)
+  date = db.Column(db.String())
+  start_time = db.Column(db.String())
+  duration = db.Column(db.String())
   description = db.Column(db.String())
   location = db.Column(db.String())
-  start_time = db.Column(db.DateTime())
-  end_time = db.Column(db.DateTime())
+  lat_long = db.Column(db.String())
   current_participant_count = db.Column(db.Integer())
   max_participant_count = db.Column(db.Integer())
   activity = db.Column(db.String())
+  equipment = db.Column(db.String())
 
-  def __init__(self, description, location, start_time, end_time, current_participant_count, max_participant_count, activity):
+  def __init__(self, date, start_time, duration, description, location, lat_long, current_participant_count, max_participant_count, activity, equipment):
+    self.date = date
+    self.start_time = start_time
+    self.duration = duration
     self.description = description
     self.location = location
-    self.start_time = start_time
-    self.end_time = end_time
+    self.lat_long = lat_long
     self.current_participant_count = current_participant_count
     self.max_participant_count = max_participant_count
     self.activity = activity
+    self.equipment = equipment
 
   def __repr__(self):
     return '<id {}>'.format(self.id)
