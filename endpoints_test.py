946023eacaa9831dab2f114d2bb666cbe982f5a3
Jordan Holtkamp <jordanholtkamp@gmail.com>
2020-04-12 18:22:22 -0400

    Add test to create event
    Co-authored-by: Phil DeLong <philjdelong@gmail.com>

--- endpoints_test.py
+++ endpoints_test.py
@@ -8,9 +8,9 @@ class TestApp(unittest.TestCase):
 		    app.config.from_object('config.TestingConfig')
 		    self.app = app.test_client()
         
-    def test_root_page(self):
-		    response = self.app.get('/')
-		    self.assertEqual(200, response.status_code)
+    # def test_root_page(self):
+		#     response = self.app.get('/')
+		#     self.assertEqual(200, response.status_code)
 
     def test_events_index_requests(self):
         headers = {'Content-Type': 'application/json'}
@@ -20,7 +20,7 @@ class TestApp(unittest.TestCase):
         self.assertEqual(response.headers['Content-Type'], 'application/json')
         
         json_data = json.loads(response.data)
-        self.assertEqual(json_data['data'][0]['activity'], 'spike ball')
+        self.assertEqual(json_data['data'][0]['activity'], 'Roller Hockey')
         
         array_of_keys = list(json_data['data'][0].keys())
         
@@ -32,20 +32,15 @@ class TestApp(unittest.TestCase):
     def test_post_events(self):
         headers = {'Content-Type': 'application/json'}
         
-        params = {'date': '2020-04-13', 'time': '1230', 'duration': '1:30', 'description': 'playing volleyball at wash park. need 4!', 'location': 'Wash Park', 'lat_long': '39.631,-104.973',  'current_participant_count': 6, 'max_participant_count': 10, 'activity': 'volley ball', 'equipment': 'net, ball'}
+        params = {'date': '2020-04-13', 
+                  'time': '1230', 
+                  'duration': '1:30', 
+                  'description': 'playing volleyball at wash park. need 4!', 'location': 'Wash Park', 
+                  'lat_long': '39.631,-104.973', 
+                  'current_participant_count': 6, 
+                  'max_participant_count': 10, 
+                  'activity': 'volley ball', 
+                  'equipment': 'net, ball'}
 
-        response = self.app.post('/api/v1/events/create', data=params, headers=headers)
+        response = self.app.post('/api/v1/events', data=params, headers=headers)
         self.assertEqual(response.status_code, 200)
-
-# class TestApp(unittest.TestCase):
-# 	def setUp(self):
-# 		app.config.from_object('config.TestingConfig')
-# 		self.app = app.test_client()
-# 	def test_root_page(self):
-# 		response = self.app.get('/')
-# 		self.assertEqual(200, response.status_code)
-# 	def test_search_endpoint(self):
-# 		response = self.app.get('/search?type=author&q=george rr martin')
-# 		self.assertEqual(200, response.status_code)
-# 		json_data = json.loads(response.data)
-# 		self.assertEqual(40, len(json_data.get('data')))
