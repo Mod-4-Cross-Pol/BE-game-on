791dc1f466b5b6fc9975089c3cac3478bd37f459
Jordan Holtkamp <jordanholtkamp@gmail.com>
2020-04-13 17:35:53 -0400

    adds new skill_level column to event

--- gardener.py
+++ gardener.py
@@ -5,21 +5,21 @@ db.create_all()
 
 event1 = Event(
 	date = '2020-11-05', 
-	start_time = '0400', 
+	start_time = '1200', 
 	duration = '3:00', 
-	description = 'Roller Hockey at City Park', 
+	description = 'Hoopin at City Park', 
 	location = 'City Park', 
 	lat_long = '39.761,-105.012',
 	current_participant_count = 6,
 	max_participant_count = 10,
-	activity = 'Roller Hockey',
+	activity = 'Basketball',
 	equipment = 'nets, puck'
 )
 db.session.add(event1)
 
 event2 = Event(
 	date = '2020-11-04', 
-	start_time = '0230', 
+	start_time = '1400', 
 	duration = '01:30', 
 	description = 'Ball kicking at the Pepsi Center', 
 	location = 'Pepsi Center', 
@@ -34,14 +34,14 @@ db.session.add(event2)
 
 event3 = Event(
 	date = '2020-11-15', 
-	start_time = '0230', 
+	start_time = '1500', 
 	duration = '01:30', 
 	description = 'Hotdog-eating competition', 
 	location = 'Pepsi Center', 
 	lat_long = '39.748,-105.009',
 	current_participant_count = 25,
 	max_participant_count = 35,
-	activity = 'Competition',
+	activity = 'Miscellaneous',
 	equipment = ''
 )
 db.session.add(event3)
