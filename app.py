73e53527f78930b2b493ea9e24ddd2fe0a6f5639
Phil DeLong <philjdelong@gmail.com>
2020-04-13 16:29:04 -0600

    Update config

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-app.config.from_object(os.getenv['APP_SETTINGS'])
+app.config.from_object("config.ProductionConfig")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 api = Api(app)
